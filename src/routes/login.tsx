498f15f19894850ea04329aca2d37d417a053613
Kim0426 <706shin1728@naver.com>
2024-08-07 00:06:23 +0900

    feat: 구글로그인

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,34 +1,79 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { LoginForm } from '@/pages/login/loginForm'
-import { useEffect } from 'react'
+import { useEffect, useContext } from 'react'
+import { UserContext } from '@/_context/userInfoContext'
+import { useNavigate } from '@tanstack/react-router'
+
+interface UserInfo {
+  name: string
+  email: string
+}
+
+interface UserContextType {
+  setUserInfo: (userInfo: UserInfo) => void
+}
 
 export const Route = createFileRoute('/login')({
   component: () => {
-    // URL의 쿼리 파라미터에서 code를 추출
     const params = new URLSearchParams(window.location.search)
+    const navigate = useNavigate()
     const code = params.get('code')
+    const context = useContext(UserContext) as UserContextType
 
     useEffect(() => {
-      const code = params.get('code')
-      console.log(code)
-    }, [typeof window !== 'undefined' && code])
+      const googleLogin = async () => {
+        if (code) {
+          try {
+            const response = await fetch(
+              `https://hayeongyou.shop/login?code=${code}`,
+              {
+                method: 'GET', // GET 메서드 사용
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+              },
+            )
+
+            if (!response.ok) {
+              throw new Error('Network response was not ok')
+            }
+
+            const data = await response.json()
+
+            sessionStorage.setItem('accessToken', data.accessToken)
+
+            if (context && context.setUserInfo) {
+              context.setUserInfo({
+                name: 'user',
+                email: '123123@naver.com',
+              })
+              navigate({ to: '/' })
+            }
+          } catch (error) {
+            console.error('Login error:', error)
+          }
+        }
+      }
+      googleLogin()
+    }, [code, context, navigate])
 
     const handleSubmit = async () => {
-      // 로그인 처리 로직
-
-      // 만약 code가 존재하면 백엔드로 전송
       if (code) {
         try {
           const response = await fetch(
             'http://your-backend-url/api/auth/google',
             {
-              method: '',
+              method: 'POST', // POST 메서드로 설정
               headers: {
                 'Content-Type': 'application/json',
               },
               body: JSON.stringify({ code }),
             },
           )
+
+          if (!response.ok) {
+            throw new Error('Network response was not ok')
+          }
 
           const data = await response.json()
           console.log('Success:', data)
